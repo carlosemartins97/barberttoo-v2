063e98b4b30553bc6a988f94725a8f6e9c129b63
Carlos Martins <carlosemartins97@gmail.com>
2022-08-22 09:56:22 -0300

    fix: loading na listagem de servicos

--- src/app/modules/dashboard/pages/servicos/servicos.component.ts
+++ src/app/modules/dashboard/pages/servicos/servicos.component.ts
@@ -23,11 +23,14 @@ export class ServicosComponent implements OnInit {
   }
 
   getServicos() {
+    this.loading = true;
     this.servicosService.getServices().then(res => {
       this.servicos = res;
       console.log(res);
+      this.loading = false;
     }).catch(error => {
       console.log(error);
+      this.loading = false;
     })
   }
 
